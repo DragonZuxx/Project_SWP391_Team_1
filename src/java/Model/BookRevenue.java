4c438b47610d1c67cb108cb28a8ea9c0cf05b7a9
dminhtan03 <dinhminhtan112@gmail.com>
2024-07-17 21:53:41 +0700

    Add WishlistView.jsp and listSelling.jsp and model BookRevenue

---
+++ src/java/Model/BookRevenue.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Admin
+ */
+public class BookRevenue {
+    private String month;
+    private String year;
+    private double totalRevenue;
+
+    public BookRevenue() {
+    }
+
+    public BookRevenue(String month, String year, double totalRevenue) {
+        this.month = month;
+        this.year = year;
+        this.totalRevenue = totalRevenue;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public double getTotalRevenue() {
+        return totalRevenue;
+    }
+
+    public void setTotalRevenue(double totalRevenue) {
+        this.totalRevenue = totalRevenue;
+    }
+
+}
